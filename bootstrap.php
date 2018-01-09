e713d7579f050342127fdcf86299e7e00a7eb6c1
Darren Miller <darren.miller@ihasco.co.uk>
2018-01-09 14:59:18 +0000

    RDS credentials

--- bootstrap.php
+++ bootstrap.php
@@ -7,7 +7,11 @@ $vars = new StdClass;
 $vars->phpversion = phpversion();
 $vars->fakename   = $faker->name;
 
-$pdo = new PDO('mysql:host=localhost;dbname=persist', 'root', 'password');
+$pdo = new PDO(
+                sprintf('mysql:host=%s;;port=%s;dbname=%s;charset=utf8',$_SERVER['RDS_HOSTNAME'],$_SERVER['RDS_PORT'],$_SERVER['RDS_DB_NAME']),
+                $_SERVER['RDS_USERNAME'],
+                $_SERVER['RDS_PASSWORD']
+            );
 $statement = $pdo->query("SELECT value FROM settings WHERE id=1");
 $row = $statement->fetch(PDO::FETCH_ASSOC);
 
