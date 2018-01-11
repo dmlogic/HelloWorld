d48acf091a5abc8a8d10298fe4f83f8de1a4a843
Darren Miller <darren.miller@ihasco.co.uk>
2018-01-11 14:07:41 +0000

    wrong path

--- deploy/setenv.php
+++ deploy/setenv.php
@@ -1,4 +1,4 @@
 <?php
 $manifest = json_decode( file_get_contents('/opt/elasticbeanstalk/deploy/manifest'),true);
 $appVer = array_keys(current($manifest['RuntimeSources']))[0];
-file_put_contents('/var/www/html/deploy/.env','APP_VER='.$appVer);
+file_put_contents('/var/www/html/public_html/.env','APP_VER='.$appVer);
