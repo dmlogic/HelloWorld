21d925ad8ae103fb4c0fe9b017c6076cee440f5b
Darren Miller <darren.miller@ihasco.co.uk>
2018-01-11 20:05:18 +0000

    try some logging

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,8 @@
 <?php
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Monolog\Formatter\LineFormatter;
+
 require 'vendor/autoload.php';
 
 $faker = Faker\Factory::create('en_GB');
@@ -6,3 +10,10 @@ $faker = Faker\Factory::create('en_GB');
 $vars = new StdClass;
 $vars->phpversion = phpversion();
 $vars->fakename   = $faker->name;
+
+$logger = new Logger('hello-world');
+$handler = new StreamHandler('/efs/logs/app.log');
+$handler->setFormatter(new LineFormatter("[%datetime%] %level_name%: %message% %context%\n",null, true, true));
+$logger->pushHandler( $handler );
+
+$logger->info('app.bootstrap',['fakename' => $vars->fakename]);
