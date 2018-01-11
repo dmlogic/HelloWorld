01956fd28ee4c51f0f01e3a2c29e481937149a95
Darren Miller <darren.miller@ihasco.co.uk>
2018-01-11 11:01:51 +0000

    Faker

--- public_html/index.php
+++ public_html/index.php
@@ -1,3 +1,20 @@
 <?php
-var_dump("v2");
+var_dump("v3");
+require '../bootstrap.php';
+?><!DOCTYPE html>
+<html>
+<head>
+    <title>Test App</title>
+</head>
+<body>
+<h1>Hello world!</h1>
+<p>
+    You're running PHP version <strong><?=$vars->phpversion?></strong>
+</p>
+<p>
+    Your composer Faker install worked, and your random name is <strong><?=$vars->fakename?></strong>
+</p>
+</body>
+</html>
+<?php
 phpinfo();
