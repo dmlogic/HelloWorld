bdde4950fe61f2402f3329dd2b6881cacbb84e54
Darren Miller <darren.miller@ihasco.co.uk>
2018-01-11 14:02:44 +0000

    Try to get manifest

---
+++ deploy/setenv.php
@@ -0,0 +1,4 @@
+<?php
+$manifest = json_decode( file_get_contents('/opt/elasticbeanstalk/deploy/manifest'),true);
+$appVer = array_keys(current($manifest['RuntimeSources']))[0];
+file_put_contents('/var/www/html/deploy/.env','APP_VER='.$appVer);
